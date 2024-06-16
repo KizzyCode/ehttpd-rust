fe60fb811d5d9675d738130274d78beb4c6f0f51
Keziah Biermann <development@kizzycode.de>
2024-06-17 01:43:58 +0200

    Add read-body method

--- src/http/requestext.rs
+++ src/http/requestext.rs
@@ -1,7 +1,7 @@
 //! Extension traits for `http::Request`
 
-use crate::{bytes::Data, error::Error, http::Request};
-use std::{path::Path, str};
+use crate::{bytes::Data, error, error::Error, http::Request};
+use std::{io::Read, path::Path, str};
 
 /// Some HTTP request extensions
 pub trait RequestExt {
@@ -18,6 +18,10 @@ pub trait RequestExt {
         T: AsRef<[u8]>;
     /// The request content length field if any
     fn content_length(&self) -> Result<Option<u64>, Error>;
+
+    /// Reads the request body into memory *if a content-length header is set* and *transfer-encoding is not chunked*;
+    /// returns `None` otherwise
+    fn read_body_data(&mut self, content_length_max: u64) -> Result<Option<Data>, Error>;
 }
 impl<'a, const HEADER_SIZE_MAX: usize> RequestExt for Request<'a, HEADER_SIZE_MAX> {
     #[cfg(target_family = "unix")]
@@ -50,12 +54,46 @@ impl<'a, const HEADER_SIZE_MAX: usize> RequestExt for Request<'a, HEADER_SIZE_MA
     fn content_length(&self) -> Result<Option<u64>, Error> {
         // Get the content length field if set
         let Some(content_length_raw) = self.field("Content-Length") else {
-            return Ok(None)
+            // Content-length field is unset
+            return Ok(None);
         };
 
         // Parse the field
         let content_length_utf8 = str::from_utf8(content_length_raw)?;
         let content_length: u64 = content_length_utf8.parse()?;
         Ok(Some(content_length))
+    }
+
+    fn read_body_data(&mut self, content_length_max: u64) -> Result<Option<Data>, Error> {
+        // Check if the transfer encoding is chunked
+        let is_chunked = self.field("Transfer-Encoding").map(|encoding| encoding.eq_ignore_ascii_case(b"chunked"));
+        let (None | Some(false)) = is_chunked else {
+            // Chunked transfer-encoding is not supported
+            return Ok(None);
+        };
+
+        // Check if a content-length header is set
+        let Some(content_length) = self.content_length()? else {
+            // Indeterminate lengths are not supported
+            return Ok(None);
+        };
+
+        // Validate content length
+        let true = content_length <= content_length_max else {
+            // Body is too large
+            return Err(error!("HTTP body is too large"));
+        };
+
+        // Read body
+        let mut body = Vec::new();
+        let body_len = self.stream.take(content_length).read_to_end(&mut body)? as u64;
+        let true = body_len == content_length else {
+            // Truncated body
+            return Err(error!("Truncated HTTP body"))?;
+        };
+
+        // Return body
+        let body = Data::from(body);
+        Ok(Some(body))
     }
 }
